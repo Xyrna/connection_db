7b2c17f74ab653549b983fc52367cd7dcf5db7f9
Xyrna <mammadov.mahammad2002@gmail.com>
2022-11-10 11:08:46 +0400

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace connection_db
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.turn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(85, 224);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // turn
+            // 
+            this.turn.BackColor = System.Drawing.Color.MediumPurple;
+            this.turn.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.turn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.turn.Location = new System.Drawing.Point(83, 84);
+            this.turn.Name = "turn";
+            this.turn.Size = new System.Drawing.Size(162, 90);
+            this.turn.TabIndex = 1;
+            this.turn.UseVisualStyleBackColor = false;
+            this.turn.Click += new System.EventHandler(this.turn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 320);
+            this.Controls.Add(this.turn);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button turn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace connection_db
+{
+    public partial class Form1 : Form
+    {
+
+
+        private SqlConnection sql = new SqlConnection("Server=DESKTOP-2QKN505\\SQLEXPRESS; Integrated Security=true;");
+        private bool isConnected = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void check()
+        {
+            if (sql.State == ConnectionState.Open)
+            {
+                label1.Text = "Connected";
+                turn.Text = "Disconnect";
+                isConnected = true;
+            }
+            else if (sql.State == ConnectionState.Closed)
+            {
+                label1.Text = "Disconnected";
+                turn.Text = "Connect";
+                isConnected = false;
+            }
+            else
+            {
+                label1.Text = "Connection error";
+                isConnected = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sql.Open();
+            check();
+        }
+
+        private void turn_Click(object sender, EventArgs e)
+        {
+            if(isConnected)
+            {
+                sql.Close();
+            }
+            else
+            {
+                sql.Open();
+            }
+            check();
+        }
+    }
+}
